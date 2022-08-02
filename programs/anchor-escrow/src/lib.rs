6461972f67541091929330216130754ea371649d
Ubuntu <ubuntu@ip-172-31-0-123.eu-central-1.compute.internal>
2022-08-02 12:58:01 +0000

    idl escrow

--- programs/anchor-escrow/src/lib.rs
+++ programs/anchor-escrow/src/lib.rs
@@ -2,7 +2,7 @@ use anchor_lang::prelude::*;
 use anchor_spl::token::{self, CloseAccount, Mint, SetAuthority, TokenAccount, Transfer};
 use spl_token::instruction::AuthorityType;
 
-declare_id!("6m7hFFRbDoCAN5bTm592crzaXNV3qkYwt6aaEzd1rkg6");
+declare_id!("8uay33TErnMNRYFrsQTGsXqK1jzSZq9hDXGvEZAACPmd");
 
 #[program]
 pub mod anchor_escrow {
